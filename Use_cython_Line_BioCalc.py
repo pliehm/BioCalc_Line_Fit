9a53ca8df0474d45d92e84732c1f277278aff0be
Philipp Liehm <pliehm@ppp187.(none)>
2013-12-17 15:48:51 +0000

    Changes in type declaration (abs()), 2x faster now

--- Use_cython_Line_BioCalc.py
+++ Use_cython_Line_BioCalc.py
@@ -22,7 +22,7 @@ tolerance=1
 # define parameters for peakdetection  
 lookahead_min = 4 # something like peak width for thr minima
 lookahead_max = 3 # for the maxima --> should not be larger than lookahead_min
-delta = 7	# something like peak height
+delta = 7    # something like peak height
 
 # chose wavelength range and step-width
 
@@ -49,19 +49,19 @@ dateien.sort()
 
 alle=np.zeros((wave_end-wave_start+1,1024,1280),np.uint16)
 def image2array(Img):
-	newArr= np.fromstring(Img.tostring(),np.uint8)
-	newArr= np.reshape(newArr, (1024,1280))
-	return newArr
+    newArr= np.fromstring(Img.tostring(),np.uint8)
+    newArr= np.reshape(newArr, (1024,1280))
+    return newArr
 
 counter=0
 for i in xrange(len(dateien)):
-	if dateien[i][-5:]=='.tiff':
-		if int(dateien[i][:3]) >= wave_start and int(dateien[i][:3]) <= wave_end:
-			print dateien[i]
-			print counter
-			Img=im.open(folder + '/' + dateien[i]).convert('L')
-			alle[counter]=image2array(Img)
-			counter+= 1
+    if dateien[i][-5:]=='.tiff':
+        if int(dateien[i][:3]) >= wave_start and int(dateien[i][:3]) <= wave_end:
+            print dateien[i]
+            print counter
+            Img=im.open(folder + '/' + dateien[i]).convert('L')
+            alle[counter]=image2array(Img)
+            counter+= 1
 
 
 # read simulation file
@@ -79,29 +79,29 @@ sim_waves=[]
 wave_block=[]
 
 for thisline in string.split('\n'):
-	if ('\t' in thisline) == False and len(thisline) != 0:
-		thickness = thisline
-	if ('\t' in thisline) == True and int(thickness) >= d_min and int(thickness) <= d_max:
-		positioncounter == 0
-		for word in thisline.split('\t'):
-			if len(word)<6 and float(word) >= wave_start +lookahead_min and float(word)<= wave_end - lookahead_min:
-				wave_block.append(float(word))
-	if len(thisline) == 0 and int(thickness) >= d_min and int(thickness) <= d_max:
-		sim_waves.append([thickness,len(wave_block),wave_block]) # calculate length of the waveblock since it will be needed later
-		wave_block=[]
+    if ('\t' in thisline) == False and len(thisline) != 0:
+        thickness = thisline
+    if ('\t' in thisline) == True and int(thickness) >= d_min and int(thickness) <= d_max:
+        positioncounter == 0
+        for word in thisline.split('\t'):
+            if len(word)<6 and float(word) >= wave_start +lookahead_min and float(word)<= wave_end - lookahead_min:
+                wave_block.append(float(word))
+    if len(thisline) == 0 and int(thickness) >= d_min and int(thickness) <= d_max:
+        sim_waves.append([thickness,len(wave_block),wave_block]) # calculate length of the waveblock since it will be needed later
+        wave_block=[]
 
 
 t_sum = 0
 
 for i in range(1):
 
-	t1 = time.time()
+    t1 = time.time()
 
-	dicke = Fit.c_Fit_Pixel(alle, zeile, sim_waves, waves, tolerance, lookahead_min, lookahead_max, delta)
+    dicke = Fit.c_Fit_Pixel(alle, zeile, sim_waves, waves, tolerance, lookahead_min, lookahead_max, delta)
 
-	t2 = time.time()
-	print t2-t1, 'Sekunden'
-	t_sum +=t2-t1
+    t2 = time.time()
+    print t2-t1, 'Sekunden'
+    t_sum +=t2-t1
 print t_sum/10
 
 
@@ -109,8 +109,8 @@ print t_sum/10
 
 p = open('boarder_line_fit_row_' + folder +  '_' + str(zeile) + '_tolerance_' + str(tolerance) + '_'+str(wave_start) + '_' + str(wave_end)  + 'nm.txt','w')
 for i in range(len(dicke)):
-	p.write(str(dicke[i])+'\n')
-p.close()	
+    p.write(str(dicke[i])+'\n')
+p.close()    
 
 # convert strings in "dicke" to integers to plot in histogram
 # make list without zeros to get real mean value
@@ -118,7 +118,7 @@ p.close()
 dicke_i = []
 
 for element in dicke:
-	dicke_i.append(int(element))
+    dicke_i.append(int(element))
 
 print 'Anzahl von Dicke = 0', dicke.count(0)
 
@@ -137,6 +137,7 @@ plt.savefig('hist_' + 'both_boarders_' + folder + '_zeile_' +  str(zeile) + '_to
 (tolerance) + '_'+str(wave_start) + '_' + str(wave_end)  + 'nm.png')
 
 plt.show()
+
 
 
 
--- cython_Line_BioCalc.pyx
+++ cython_Line_BioCalc.pyx
@@ -8,174 +8,176 @@ DTYPE = np.uint16
 ctypedef np.uint16_t DTYPE_t
 
 
-
 # fit simulated and experimental minima to calculate thickness
 # problem: speed!! --> one main problem is the access of the large list sim_waves --> can this be written as an array?
 
+cdef inline float _abs(float a): return a if a>=0 else -a
 
 @cython.boundscheck(False)
 cdef Fit(list sim_waves, list exp_waves, int tolerance):
 
 # cython type definition of the used objects
  
-	cdef list sim_min_waves = [[1000],[1000]] # dummy values to have two lists
-	cdef unsigned short i, k
-	cdef unsigned short L_exp_waves = len(exp_waves) # too not use the len() function too often
-	cdef  float summe=0
-	cdef list results=[]
+    cdef list sim_min_waves = [[1000],[1000]] # dummy values to have two lists
+    cdef unsigned short i, k
+    cdef unsigned short L_exp_waves = len(exp_waves) # too not use the len() function too often
+    cdef  float summe=0
+    if L_exp_waves > 2: # first test if there are more than two minima
+        for i in xrange(len(sim_waves)): # do the following calculations for every simulated thickness
+            if sim_waves[i][1] == L_exp_waves: # case for equal minimas (exp, sim)
+                summe=0
+                # perform something like least-square with every exp-wavelength 
+                #summe_l = [abs(sim_waves[i][2][k]-exp_waves[k]) for k in xrange(L_exp_waves)]
 
-	if L_exp_waves > 2: # first test if there are more than one minima
-		for i in xrange(len(sim_waves)): # do the following calculations for every simulated thickness
-			if sim_waves[i][1] == L_exp_waves: # case for equal minimas (exp, sim)
-				summe=0
-# perform something like least-square with every exp-wavelength 
-				for k in xrange(len(exp_waves)): 
-					summe+= abs(sim_waves[i][2][k]-exp_waves[k])
-# append the thickness and error to a list
-				sim_min_waves[0].append(sim_waves[i][0])
-				sim_min_waves[1].append(summe/float(len(exp_waves)))
-# do the same if number of exp and sim is not equal
-			if sim_waves[i][1] == (L_exp_waves + 1):
-				summe=0
-# check if the first elements (exp and sim) or the last tow are not matching
-				if abs(sim_waves[i][2][0] - exp_waves[0]) > abs(sim_waves[i][2][-1]-exp_waves[-1]):
-					for k in xrange(len(exp_waves)):
-						summe+= abs(sim_waves[i][2][k+1]-exp_waves[k])
-					sim_min_waves[0].append(sim_waves[i][0])
-					sim_min_waves[1].append(summe/float(len(exp_waves)))
-				else:
-					for k in xrange(len(exp_waves)):
-						summe+= abs(sim_waves[i][2][k]-exp_waves[k])
-					sim_min_waves[0].append(sim_waves[i][0])
-					sim_min_waves[1].append(summe/float(len(exp_waves)))
-			if sim_waves[i][1] == (L_exp_waves - 1):
+                for k in xrange(L_exp_waves): 
+                    summe+= _abs(sim_waves[i][2][k]-exp_waves[k])
+                # append the thickness and error to a list
+                sim_min_waves[0].append(sim_waves[i][0])
+                sim_min_waves[1].append(summe/float(L_exp_waves))
+                continue
 
-				summe=0
-				if abs(sim_waves[i][2][0] - exp_waves[0]) > abs(sim_waves[i][2][-1]-exp_waves[-1]):
-					for k in xrange(sim_waves[i][1]):
-						summe+= abs(sim_waves[i][2][k]-exp_waves[k+1])
-					sim_min_waves[0].append(sim_waves[i][0])
-					sim_min_waves[1].append(summe/float(len(exp_waves)))
-				else:
-					for k in xrange(sim_waves[i][1]):
-						summe+= abs(sim_waves[i][2][k]-exp_waves[k])
-					sim_min_waves[0].append(sim_waves[i][0])
-					sim_min_waves[1].append(summe/float(len(exp_waves)))
-				
-# append results to a list for further processing
-		#results=[]
-		#results=[sim_min_waves[i][1] for  i in xrange(len(sim_min_waves))]
+            # do the same if number of exp and sim is not equal
+            if sim_waves[i][1] == (L_exp_waves + 1):
+                summe=0
+                # check if the first elements (exp and sim) or the last tow are not matching
+                if _abs(sim_waves[i][2][0] - exp_waves[0]) > _abs(sim_waves[i][2][-1]-exp_waves[-1]):
+                    for k in xrange(L_exp_waves):
+                        summe+= _abs(sim_waves[i][2][k+1]-exp_waves[k])
+                    sim_min_waves[0].append(sim_waves[i][0])
+                    sim_min_waves[1].append(summe/float(L_exp_waves))
+                    continue
+                else:
+                    for k in xrange(L_exp_waves):
+                        summe+= _abs(sim_waves[i][2][k]-exp_waves[k])
+                    sim_min_waves[0].append(sim_waves[i][0])
+                    sim_min_waves[1].append(summe/float(L_exp_waves))
+                    continue
 
+            if sim_waves[i][1] == (L_exp_waves - 1):
+
+                summe=0
+                if _abs(sim_waves[i][2][0] - exp_waves[0]) > _abs(sim_waves[i][2][-1]-exp_waves[-1]):
+                    for k in xrange(sim_waves[i][1]):
+                        summe+= _abs(sim_waves[i][2][k]-exp_waves[k+1])
+                    sim_min_waves[0].append(sim_waves[i][0])
+                    sim_min_waves[1].append(summe/float(L_exp_waves))
+                    continue
+                else:
+                    for k in xrange(sim_waves[i][1]):
+                        summe+= _abs(sim_waves[i][2][k]-exp_waves[k])
+                    sim_min_waves[0].append(sim_waves[i][0])
+                    sim_min_waves[1].append(summe/float(L_exp_waves))
+                    continue
+                
 # return the thickness with minimum value
-#		if  len(sim_min_waves[1])>1 and (min(results) < tolerance):
-#			return sim_min_waves[ results.index(min(results))][0]
-		if  len(sim_min_waves[0])>1 and (min(sim_min_waves[1]) < tolerance):
-			return sim_min_waves[0][sim_min_waves[1].index(min(sim_min_waves[1]))]
-		else: 
-			return 0
+        if  len(sim_min_waves[0])>1 and (min(sim_min_waves[1]) < tolerance):
+            return sim_min_waves[0][sim_min_waves[1].index(min(sim_min_waves[1]))]
+        else: 
+            return 0
 
-	else:
-		return 0
+    else:
+        return 0
 
 # function to get minima of one array
 
 
 cdef peakdetect(y_axis, x_axis = None, unsigned short lookahead_min=5, unsigned short lookahead_max=3, unsigned short delta = 0):
-	
-	# define output container
-	#cdef list max_peaks=[]
-	cdef list min_peaks = []
-	cdef list dump = [] # used to pop the first hit which almost always is false
+    
+    # define output container
+    #cdef list max_peaks=[]
+    cdef list min_peaks = []
+    cdef list dump = [] # used to pop the first hit which almost always is false
 
-	# check input data --> this makes the algorithm 5 times slower
-	#x_axis, y_axis = _datacheck_peakdetect(x_axis, y_axis) 
-	
-	# store data length for later use
-	cdef int length = len(y_axis)
+    # check input data --> this makes the algorithm 5 times slower
+    #x_axis, y_axis = _datacheck_peakdetect(x_axis, y_axis) 
+    
+    # store data length for later use
+    cdef int length = len(y_axis)
 
-	#perform some checks
-	#if lookahead < 1:
-	#	raise ValueError, "Lookahead must be '1' or above in value"
-	#if not (np.isscalar(delta) and delta >= 0):
-	#	raise ValueError, "delta must be a positive number"
+    #perform some checks
+    #if lookahead < 1:
+    #    raise ValueError, "Lookahead must be '1' or above in value"
+    #if not (np.isscalar(delta) and delta >= 0):
+    #    raise ValueError, "delta must be a positive number"
 
-	#maxima and minima candidates are temporarily stored in 
-	#mx and mn respectively
-	cdef int mn = 1000
-	cdef int mx = -1000
-	cdef unsigned short x,y, index
+    #maxima and minima candidates are temporarily stored in 
+    #mx and mn respectively
+    cdef int mn = 1000
+    cdef int mx = -1000
+    cdef unsigned short x,y, index
 
-	for index, (x,y) in enumerate(zip(x_axis[:-lookahead_min], y_axis[:-lookahead_min])):
-		
-		if y > mx:
-			mx = y
-			mxpos = x
+    for index, (x,y) in enumerate(zip(x_axis[:-lookahead_min], y_axis[:-lookahead_min])):
+        
+        if y > mx:
+            mx = y
+            mxpos = x
 
-		if y < mn:
-			mn = y
-			mnpos = x
+        if y < mn:
+            mn = y
+            mnpos = x
 
-		#### look for max ####
-		
-		if y < mx-delta and mx != 1000:
-			#Maxima peak candidate found
-			# lool ahead in signal to ensure that this is a peak and not jitter
-			if y_axis[index:index+lookahead_max].max() < mx:
-				#max_peaks.append([mxpos, mx])
-				dump.append(True)
-				#set algorithm to only find minima now
-				
-				mx = 1000
-				mn = 1000
-				if index+lookahead_min >= length:
-					#end is within lookahead no more peaks can be found
-					break
-				continue
+        #### look for max ####
+        
+        if y < mx-delta and mx != 1000:
+            #Maxima peak candidate found
+            # lool ahead in signal to ensure that this is a peak and not jitter
+            if y_axis[index:index+lookahead_max].max() < mx:
+                #max_peaks.append([mxpos, mx])
+                dump.append(True)
+                #set algorithm to only find minima now
+                
+                mx = 1000
+                mn = 1000
+                if index+lookahead_min >= length:
+                    #end is within lookahead no more peaks can be found
+                    break
+                continue
 
-		#### look for min ####	
-		
-		if y > mn+delta and mn != -1000:
-			#Minima peak candidate found
-			# look ahead in signal to ensure that this is a peak and not jitter
-			if y_axis[index:index+lookahead_min].min() > mn:
-				min_peaks.append(mnpos)
-				dump.append(False)
-				#set algorithm to only find maximum now
-				mn = -1000
-				mx = -1000
-				if index+lookahead_min >= length:
-					#end is within lookahead no more peaks can be found
-					break
+        #### look for min ####    
+        
+        if y > mn+delta and mn != -1000:
+            #Minima peak candidate found
+            # look ahead in signal to ensure that this is a peak and not jitter
+            if y_axis[index:index+lookahead_min].min() > mn:
+                min_peaks.append(mnpos)
+                dump.append(False)
+                #set algorithm to only find maximum now
+                mn = -1000
+                mx = -1000
+                if index+lookahead_min >= length:
+                    #end is within lookahead no more peaks can be found
+                    break
 
 
-	#Remove the false hit on the first value of the y_axis
-	if len(dump)>0:
-		if not dump[0]:
-			min_peaks.pop(0)
-		else:
-			pass	
-	
-		#no peaks were found, should the function return empty lists?
-	
-	return min_peaks
+    #Remove the false hit on the first value of the y_axis
+    if len(dump)>0:
+        if not dump[0]:
+            min_peaks.pop(0)
+        else:
+            pass    
+    
+        #no peaks were found, should the function return empty lists?
+    
+    return min_peaks
 
-	
+    
 # find reflection minima for every pixel
 
 def c_Fit_Pixel(np.ndarray[DTYPE_t, ndim=3] data, int zeile, list sim_waves, list waves, int tolerance, unsigned short lookahead_min,unsigned short lookahead_max, unsigned short delta):
-	cdef unsigned short spalte
-	cdef np.ndarray[DTYPE_t, ndim=1] intensity
-	cdef list minima_exp=[]
-	cdef list thickness_ready=[]
-	cdef int counter=0
-	for spalte in xrange(1280):
-		intensity = data[:,zeile, spalte]
-		minima_exp = peakdetect(intensity, waves, lookahead_min,lookahead_max, delta)
-		#print minima_exp
-		thickness_ready.append(Fit(sim_waves, minima_exp,tolerance))
-		#print counter		
-		counter+=1
-		
-	return thickness_ready
-	
+    cdef unsigned short spalte
+    cdef np.ndarray[DTYPE_t, ndim=1] intensity
+    cdef list minima_exp=[]
+    cdef list thickness_ready=[]
+    cdef int counter=0
+    # build another sim_waves_m list with different list class
 
+    for spalte in xrange(1280):
+        intensity = data[:,zeile, spalte]
+        minima_exp = peakdetect(intensity, waves, lookahead_min,lookahead_max, delta)
+        #print minima_exp
+        thickness_ready.append(Fit(sim_waves, minima_exp,tolerance))
+        #print counter        
+        counter+=1
+        
+    return thickness_ready
+    
